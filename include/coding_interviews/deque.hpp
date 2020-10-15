093a67555a4afcff69d1bd97a22953fb754e10e1
Peyton Seigo <pseigo@yahoo.com>
2020-10-15 02:29:48 -0600

    Bug fixes; pop_back(); helpers; started resize()

--- include/coding_interviews/deque.hpp
+++ include/coding_interviews/deque.hpp
@@ -4,7 +4,8 @@
 #include <memory>
 
 /**
- * Double-ended queue implemented with a growable circular buffer.
+ * Double-ended queue implemented with a growable circular buffer. Not thread
+ * safe.
  */
 namespace PAJS
 {
@@ -25,8 +26,11 @@ namespace PAJS
     // const T& front() const;
     // const T& at(size_t index) const;
 
-  private:
     bool empty() const;
+    size_t size() const;
+    size_t max_size() const;
+
+  private:
     bool full() const;
     size_t offsetIndex(size_t index, ssize_t offset) const;
     void expand();
@@ -34,7 +38,8 @@ namespace PAJS
 
     static constexpr size_t M_GROWTH_FACTOR = 2;
 
-    size_t m_size;
+    size_t m_max_size;
+    size_t m_actual_max_size;
     std::unique_ptr<T[]> m_array;
     size_t m_frontIndex;
     size_t m_backIndex;
--- src/data_structures/deque.cpp
+++ src/data_structures/deque.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
+#include <deque>
 #include "deque.hpp"
 
 int main()
 {
-  PAJS::Deque<int> d(5);
+  PAJS::Deque<int> d(6);
 
-  for (int i = 1; i < 6; i++) {
+  for (int i = 1; i < 8; i++) {
     d.push_back(i);
     const int& elem = d.back();
-    std::cout << "Back: " << elem << " (pushed " << i << ")\n";
+    std::cout << "Back: " << elem << " (pushed " << i
+      << ". size: " << d.size()
+      << ". max size: " << d.max_size() << ")\n";
   }
   std::cout << std::flush;
 
+  for (int i = 0; i < 10; i++) {
+    d.pop_back();
+    const int& elem = d.back();
+    std::cout << "New back: " << elem << " (size: " << d.size()
+      << ". max size: " << d.max_size() << ")\n";
+  }
+  std::cout << std::flush;
+
+  // Testing the std deque.
+  // std::deque<int> sd;
+  // sd.push_back(42);
+  // std::cout << &(sd.back()) << std::endl;
+  // sd.pop_back();
+  // std::cout << &(sd.back()) << std::endl;
+  // std::cout << &(sd.back()) << std::endl;
+  // sd.pop_back();
+  // std::cout << &(sd.back()) << std::endl;
 }
 
 namespace PAJS
 {
   /* Public interface */
   template <typename T>
-  Deque<T>::Deque(size_t initial_size) : m_size{initial_size}
+  Deque<T>::Deque(size_t initial_size) :
+    m_max_size{initial_size},
+    m_actual_max_size{initial_size + 1}
   {
-    m_frontIndex = 0;
-    m_backIndex = 0;
+    m_frontIndex = 3;
+    m_backIndex = 3;
 
     if (initial_size) {
-      m_array = std::make_unique<T[]>(initial_size);
-      std::fill_n(m_array.get(), initial_size, 0);
+      m_array = std::make_unique<T[]>(m_actual_max_size);
+      std::fill_n(m_array.get(), m_actual_max_size, 0);
     } else {
       m_array = nullptr;
     }
@@ -35,11 +57,26 @@ namespace PAJS
   void Deque<T>::push_back(const T& val)
   {
     if (full()) {
-      expand();
+      // TODO: Uncomment expand() once implemented.
+      // expand();
+      return;
     }
 
     m_array[m_backIndex] = val;
     m_backIndex = offsetIndex(m_backIndex, 1);
+  }
+
+  template <typename T>
+  void Deque<T>::pop_back()
+  {
+    if (empty()) {
+      return;
+    }
+
+    // TODO: Memory leak? Is there some way to "free" a generic element and
+    // is that something we'd even want to do? We could just leave it in
+    // memory.
+    m_backIndex = offsetIndex(m_backIndex, -1);
   }
 
   template <typename T>
@@ -54,28 +91,46 @@ namespace PAJS
     return m_array[nindex];
   }
 
-    /* Private helpers */
   template <typename T>
   bool Deque<T>::empty() const
   {
-    return (m_array == nullptr) || (m_size == 0);
+    return (m_array == nullptr) || (size() == 0);
   }
 
   template <typename T>
+  size_t Deque<T>::size() const
+  {
+    if (m_frontIndex <= m_backIndex) {
+      return m_backIndex - m_frontIndex;
+    } else {
+      return m_actual_max_size - (m_frontIndex - m_backIndex);
+    }
+    return 0;
+  }
+
+  template <typename T>
+  size_t Deque<T>::max_size() const
+  {
+    return m_max_size;
+  }
+
+  /* Private helpers */
+  template <typename T>
   bool Deque<T>::full() const
   {
-    return (m_array != nullptr) && (m_frontIndex == m_backIndex);
+    size_t fullIndex = offsetIndex(m_frontIndex, -1);
+    return (m_array != nullptr) && (m_backIndex == fullIndex);
   }
 
   // TODO: CLean up
   template <typename T>
   size_t Deque<T>::offsetIndex(size_t index, ssize_t offset) const
   {
-    const size_t steps = abs(offset) % m_size;
+    const size_t steps = abs(offset) % m_actual_max_size;
 
     if (offset > 0) {
-      if (index + steps >= m_size) { // Will wrap around
-        const size_t distanceToEnd = m_size - index - 1;
+      if (index + steps >= m_actual_max_size) { // Will wrap around
+        const size_t distanceToEnd = m_actual_max_size - index - 1;
         const size_t stepsForwardFromFront = steps - distanceToEnd - 1;
         index = stepsForwardFromFront;
       } else { // Will not wrap around
@@ -84,7 +139,7 @@ namespace PAJS
     } else if (offset < 0) {
       if (steps > index) { // Will wrap around
         size_t stepsBackwardFromEnd = steps - index;
-        index = m_size - stepsBackwardFromEnd;
+        index = m_actual_max_size - stepsBackwardFromEnd;
       } else { // Will not wrap around
         index -= steps;
       }
@@ -96,12 +151,24 @@ namespace PAJS
   template <typename T>
   void Deque<T>::expand()
   {
-    resize(m_size * M_GROWTH_FACTOR);
+    resize(m_max_size * M_GROWTH_FACTOR + 1);
   }
 
+  /* Takes O(size) time and space. */
   template <typename T>
   void Deque<T>::resize(size_t size)
   {
-    // TODO
+    // TODO: Implement iterators so I don't have to struggle over this.
+    if (size >= 0) {
+      std::unique_ptr<T[]> expanded = std::make_unique<T[]>(size);
+
+      for (size_t i = 0; i < m_actual_max_size; i++) {
+        expanded[i] = m_array[i];
+      }
+
+      m_array = std::move(expanded);
+      m_actual_max_size = size;
+      m_max_size = m_actual_max_size - 1;
+    }
   }
 }
