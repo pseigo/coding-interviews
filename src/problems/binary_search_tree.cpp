b4453c00af0995126a91997c0326786ddb1323d7
Peyton Seigo <pseigo@yahoo.com>
2020-11-05 00:53:58 -0700

    Add binary search tree algorithms

---
+++ src/problems/binary_search_tree.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+struct Node {
+    Node *left; 
+    Node *right;
+    T value;
+};
+
+template <typename T>
+Node<T>* create_node(const T &value)
+{
+    Node<T> *node = new Node<T>;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->value= value;
+    return node; 
+}
+
+// Inserts a node in the BST rooted by `root` and returns it.
+template <typename T>
+Node<T>* insert(Node<T> *&root, const T &value)
+{
+    if (root == nullptr) {
+        root = create_node(value);
+        return root;
+    }
+
+    // Duplicate value
+    if (root->value == value) {
+        if (root->left == nullptr) {
+            root->left = create_node(value); 
+            return root->left;
+        } else if (root->right == nullptr) {
+            root->right = create_node(value); 
+            return root->right;
+        } else {
+            return insert(root->left, value);
+        }
+    }
+
+    if (value < root->value) {
+        return insert(root->left, value);
+    } else {
+        return insert(root->right, value);
+    }
+}
+
+template <typename T>
+void print_preorder(Node<T> *root)
+{
+    if (root == nullptr) {
+        return;
+    }
+    std::cout << root->value << std::endl;
+    print_inorder(root->left);
+    print_inorder(root->right);
+    return;
+}
+
+template <typename T>
+void print_inorder(Node<T> *root)
+{
+    if (root == nullptr) {
+        return;
+    }
+    print_inorder(root->left);
+    std::cout << root->value << std::endl;
+    print_inorder(root->right);
+    return;
+}
+
+template <typename T>
+void print_postorder(Node<T> *root)
+{
+    if (root == nullptr) {
+        return;
+    }
+    print_inorder(root->left);
+    print_inorder(root->right);
+    std::cout << root->value << std::endl;
+    return;
+}
+
+template <typename T>
+void delete_tree(Node<T> *root)
+{
+    if (root == nullptr) {
+        return;
+    }
+    delete_tree(root->left);
+    delete_tree(root->right);
+    delete root;
+    return;
+}
+
+// Performs a depth-first search for a node with the `value`. Returns nullptr if
+// such a node does not exist in the BST rooted by `root`.
+template <typename T>
+Node<T>* dfs(Node<T> *root, const T &value)
+{
+    if (root == nullptr) return nullptr;
+    else if (value < root->value) return dfs(root->left, value);
+    else if (value > root->value) return dfs(root->right, value);
+    else return root;
+}
+
+int main()
+{
+    Node<int> *root = create_node<int>(10);
+
+    std::vector<int> values = {15, 5, 7, 5, 3, 20, 2, -1};
+    for (const int &i : values) {
+        insert<int>(root, i);
+    }
+
+    std::cout << "===================\n"
+              << "Preorder traversal:" << std::endl;
+    print_preorder(root);
+
+    std::cout << "==================\n"
+              << "Inorder traversal:" << std::endl;
+    print_inorder(root);
+
+    std::cout << "====================\n"
+              << "Postorder traversal:" << std::endl;
+    print_postorder(root);
+
+    for (const int &search_value : values) {
+        Node<int> *dfs_result = dfs(root, search_value);
+        if (dfs_result == nullptr) {
+            std::cout << "Did not find the value "
+                << search_value << " in the BST." << std::endl;
+        } else {
+            std::cout << "Found the value " << search_value
+                << " in the BST. Sanity check: the node's value is also "
+                << dfs_result->value << "." << std::endl;
+        }
+    }
+
+    delete_tree(root);
+}
